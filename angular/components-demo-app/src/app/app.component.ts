6e8ee8c0fdc961b3798499d830c82e34ed70caf3
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2023-06-24 10:50:45 +0300

    Just going through the project!

--- angular/components-demo-app/src/app/app.component.ts
+++ angular/components-demo-app/src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // template: '<h1>Test from template</h1>',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // styles: [`div {
+  //   background: red;
+  // }`],
 })
 export class AppComponent {
-  title = 'components-demo-app';
+  title = 'title from AppComponent';
+  hello = 'Hello World';
 }
