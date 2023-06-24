7988cf285c2f79eb309ce156f0ca81b550fb17f0
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2023-06-24 13:27:38 +0300

    Component Test!

--- angular/components-demo-app/src/app/app.component.ts
+++ angular/components-demo-app/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   // template: '<h1>Test from template</h1>',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html', // .html template
+  styleUrls: ['./app.component.css'], // .css template
   // styles: [`div {
   //   background: red;
   // }`],
--- angular/components-demo-app/src/app/app.module.ts
+++ angular/components-demo-app/src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    // add components
+    AppComponent,
+    HomeComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule
