3ea0c2e0ebf79886fc453a167bc6f6ebc327af21
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2023-07-03 23:31:18 +0300

    Adding routing module!

--- angular/lec3-demo-app/src/app/app.module.ts
+++ angular/lec3-demo-app/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListItemComponent } from './user-list-item/user-list-item.component';
 import { UsersListComponent } from './users-list/users-list.component';
 import { UserService } from './user.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { UserService } from './user.service';
     UsersListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [ UserService ], // imported for mocha test, injected in the app module
   bootstrap: [AppComponent]
