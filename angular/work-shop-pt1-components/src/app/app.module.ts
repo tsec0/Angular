eabe7ba56ff7e0ff2437d4df940ec1e1ad714bb3
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2023-07-05 12:37:01 +0300

    Real Work Shop component start !

---
+++ angular/work-shop-pt1-components/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { MainComponent } from './main/main.component';
+import { ThemesListComponent } from './themes-list/themes-list.component';
+import { PostsListComponent } from './posts-list/posts-list.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    ThemesListComponent,
+    PostsListComponent
+  ],
+  imports: [
+    CoreModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
