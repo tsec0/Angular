664b615e9673dc07c96e449803ccf48161c43736
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2023-07-16 15:05:16 +0300

    Update login component

--- angular/work-shop-pt1-components/src/app/user/login/login.component.ts
+++ angular/work-shop-pt1-components/src/app/user/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
@@ -10,10 +10,15 @@ import { DEFAULT_EMAIL_DOMAINS } from 'src/app/shared/constants';
   styleUrls: ['./login.component.css']
 })
 
-export class LoginComponent {
+export class LoginComponent { // implements OnDestroy {
   appEmailDomains = DEFAULT_EMAIL_DOMAINS;
 
   constructor(private userService: UserService, private router: Router){}
+
+  // ngOnDestroy(): void {
+  //   console.log("NGONDESTROY INVOKED!")
+  //   // throw new Error('Method not implemented.');
+  // }
 
   login(form: NgForm): void {
 
